87049f98cdd228a72a5d4ae8b9b89fc4e69fd09d
amir dadipoor <amirdadipoor@hotmail.com>
2025-05-25 14:51:44 +0330

    do small modifications

--- refactor/debug.js
+++ refactor/debug.js
@@ -5,20 +5,10 @@ const grok = require('../lib');
 const p = '%{IP:client} \\[%{TIMESTAMP_ISO8601:timestamp}\\] "%{WORD:method} %{URIHOST:site}%{URIPATHPARAM:url}" %{INT:code} %{INT:request} %{INT:response} - %{NUMBER:took} \\[%{DATA:cache}\\] "%{DATA:mtag}" "%{DATA:agent}"';
 const str = '203.35.135.165 [2016-03-15T12:42:04+11:00] "GET memz.co/cloud/" 304 962 0 - 0.003 [MISS] "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"';
 
-grok.loadDefault((err, patterns) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    const pattern = patterns.createPattern(p);
-
-    pattern.parse(str, (err, obj) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        console.log(obj);
-    });
-});
+grok.loadDefault().then(patterns => {
+    return patterns.createPattern(p).parse(str);
+}).then(obj => {
+    console.log(obj);
+}).catch(err => {
+    console.log(err);
+})
