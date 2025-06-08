649c0f501aea7e6a62897c375a1d53a0eebea7ef
amir dadipoor <amirdadipoor@hotmail.com>
2025-06-08 15:07:07 +0330

    modify add project

---
+++ old/index.d.ts
@@ -0,0 +1,12 @@
+export class GrokPattern {
+  parse(str: string, callback: { (err: any, result: any): any }): any;
+  parseSync(str: string): any;
+}
+
+export class GrokCollection {
+  createPattern(expression: string, id?: string): GrokPattern;
+  getPattern(id: string): GrokPattern;
+  load(filePath: string, callback: { (err?: any): any }): any;
+  loadSync(filePath: string): number;
+  count(): number;
+}
--- refactor/debug.js
+++ refactor/debug.js
@@ -2,8 +2,11 @@ const grok = require('../lib');
 
 
 
-const p = '%{IP:client} \\[%{TIMESTAMP_ISO8601:timestamp}\\] "%{WORD:method} %{URIHOST:site}%{URIPATHPARAM:url}" %{INT:code} %{INT:request} %{INT:response} - %{NUMBER:took} \\[%{DATA:cache}\\] "%{DATA:mtag}" "%{DATA:agent}"';
-const str = '203.35.135.165 [2016-03-15T12:42:04+11:00] "GET memz.co/cloud/" 304 962 0 - 0.003 [MISS] "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"';
+//const p = '%{IP:client} \\[%{TIMESTAMP_ISO8601:timestamp}\\] "%{WORD:method} %{URIHOST:site}%{URIPATHPARAM:url}" %{INT:code} %{INT:request} %{INT:response} - %{NUMBER:took} \\[%{DATA:cache}\\] "%{DATA:mtag}" "%{DATA:agent}"';
+// str = '203.35.135.165 [2016-03-15T12:42:04+11:00] "GET memz.co/cloud/" 304 962 0 - 0.003 [MISS] "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"';
+
+const p = '%{SYSLOGTIMESTAMP:date_time}%{DATA}';
+const str = 'Jun  7 06:45:07 172.20.54.33 %ASA-6-302020: Built inbound ICMP connection for faddr 172.20.50.2/0 gaddr 172.20.50.152/454 laddr 172.20.50.152/454 type 0 code 0 ';
 
 const parser = async ( ) => {
     try {
